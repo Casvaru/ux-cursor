64bf984897a31a45114faa8845a19b39fc9027ad
Carlos Segura <carlosandres.segurac@gmail.com>
2024-02-23 19:15:55 -0500

    Se agreggó nueva personalización al cursor pasandole un obj, se actualizó el README con las novedades

--- README.md
+++ README.md
@@ -8,5 +8,18 @@
 
 ```js
 import UxCursor from './cursor.js';
-UxCursor
+UxCursor({}) // Valores por defecto
+```
+
+## Cambio de estilo (Props)
+
+```js
+UxCursor({
+  size = 30, // Cambio del ancho y alto del cursor. Por defecto está en 25
+  mixBlend = 'none', // filtro mixBlend. Por defecto está en exclusion
+  borderRadius = '20px', //Cambiar el border radius. Por defecto está en 50%
+  color = '#2E95D3', // Cambiar el color de fondo del curosr. Por defecto es blanco
+  border = '1px solid #000', // Agrega un borde al cursor. Por defecto es none 
+  opacity = '50%' // Cambia la opacidad del cursor. Por defecto es 100%
+})
 ```
--- cursor.js
+++ cursor.js
@@ -1,27 +1,44 @@
 // @ts-check
-
 /**
- * Función para manejar un cursor personalizado que sigue la posición del mouse.
+ * Función que crea un cursor circular y sigue la posición del mouse.
+ * @example UxCursor({}) // Importación con los valores por defecto
+ * @example UxCursor({
+ *  size:40 // Cambia el tamaño del cursor a 40px
+ * })
+ * @param {Object} options - Opciones para personalizar el cursor.
+ * @param {Number} [options.size=25] - Cambia el tamaño del círculo. Por defecto está en 25.
+ * @param {String} [options.mixBlend='exclusion'] - Define el modo de mezcla CSS para el cursor. Por defecto está en 'exclusion'.
+ * @param {String} [options.borderRadius='50%'] - Define el radio de borde CSS para el cursor. Por defecto está en '50%'.
+ * @param {String} [options.color='#fff'] - Define el color del cursor. Por defecto está en '#fff'.
+ * @param {String} [options.border='none'] - Define el borde del cursor. Por defecto está en 'none'.
+ * @param {String} [options.opacity='100%'] - Define la opacidad del cursor. Por defecto está en 100%.
+ * @returns {HTMLElement} - Elemento HTML que representa el cursor.
  */
-export default function uxCursor () {
-  /**
-   * Crea un custom cursor circular que sigue el puntero del mouse.
-   * @param {number} size En las siguientes actualizaciones se le dará parámetros personalizados al cursor, NO usar.
-   * @returns HTMLElement
-  */
-  const createCursor = (size = 25) => {
+export default function uxCursor (options) {
+  const {
+    size = 25,
+    mixBlend = 'exclusion',
+    borderRadius = '50%',
+    color = '#fff',
+    border = 'none',
+    opacity = '100%'
+  } = options
+
+  const createCursor = () => {
     const cursor = document.createElement('div')
     cursor.id = 'custom-uxcursor'
     cursor.style.width = `${size}px`
     cursor.style.height = `${size}px`
-    cursor.style.borderRadius = '50%'
-    cursor.style.backgroundColor = '#fff'
+    cursor.style.borderRadius = borderRadius
+    cursor.style.backgroundColor = color
     cursor.style.position = 'fixed'
+    cursor.style.opacity = opacity
     cursor.style.zIndex = '1000'
     cursor.style.pointerEvents = 'none'
+    cursor.style.border = border
     cursor.style.transition = 'width 0.3s ease, height 0.1s ease'
     cursor.style.transform = 'translate3d(-50%,-50%,0)'
-    cursor.style.mixBlendMode = 'exclusion'
+    cursor.style.mixBlendMode = mixBlend
     document.body.appendChild(cursor)
     return cursor
   }
@@ -37,21 +54,36 @@ export default function uxCursor () {
     cursor.style.left = `${mouseX}px`
   }
   /**
-   * Función anónima que tiene una condicional si el target del mouse es igual a un link o tagName A para cambiar su estilo.
+   * Función anónima que tiene una condicional si el target del mouse es tipo hover o tiene una clase en específico y realizar el cambio de estilo al cursor.
    * @param {MouseEvent} event
    */
-  const isHoverTagA = (event) => {
-    if (event.target.tagName === 'A') {
-      cursor.style.width = '40px'
-      cursor.style.height = '40px'
+  const isHover = (event) => {
+    const eventDocument = event.target
+
+    const hoverClass = 'hover'
+    const tagName = eventDocument.tagName.toLowerCase()
+    const targetClasses = eventDocument.classList
+    const computedStyle = window.getComputedStyle(eventDocument)
+
+    // Verificar si el elemento tiene cursor: pointer en sus estilos.
+    const hasPointerCursor = computedStyle.getPropertyValue('cursor') === 'pointer'
+
+    if (
+      tagName === 'button' ||
+      tagName === 'input' ||
+      targetClasses.contains(hoverClass) ||
+      hasPointerCursor
+    ) {
+      cursor.style.width = `${size + 15}px`
+      cursor.style.height = `${size + 15}px`
     } else {
-      cursor.style.width = '25px'
-      cursor.style.height = '25px'
+      cursor.style.width = `${size}px`
+      cursor.style.height = `${size}px`
     }
   }
 
   document.addEventListener('mousemove', updateCursorPosition)
-  document.addEventListener('mousemove', isHoverTagA)
-}
+  document.addEventListener('mousemove', isHover)
 
-uxCursor()
+  return createCursor()
+}
